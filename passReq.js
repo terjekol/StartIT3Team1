a5aaa786724c173486e9d033ccb25b6ea03337af
Zebking <43807255+Zebking@users.noreply.github.com>
2018-10-10 12:32:33 +0200

    Create passReq.js

---
+++ passReq.js
@@ -0,0 +1,16 @@
+
+    <div class="backheadpassreq">Password recovery</div>
+    <div class="backtop"></div>
+
+    <div onclick="showfrontPage()" class="backbuttonsymbol">&lt;&lt</div>
+    <div onclick="showfrontPage()" class="backbutton"></div>
+
+
+    <div class="passreqfieldbox">
+        <form action="#">
+            Type inn your email address,<br>You will then get a password recovery link on email. <br />
+            <input type="text" placeholder="Email" id="passreqfield" /><br />
+
+            <button class="preqfiledsub" type="submit">Submit</button>
+        </form>
+    </div>
