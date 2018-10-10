719da7246e23afef16663c02666d3d0f2f4b5763
Ravnster <magnus195@yahoo.no>
2018-10-10 09:40:47 +0200

    Update frontPage.js

--- frontPage.js
+++ frontPage.js
@@ -5,7 +5,7 @@ function showfrontPage() {
     <div class="frontpage">
         <div class="info" onclick="showInfoPage()">i</div>
 
-        <img class="jotronlogo" src="jotron_logo.jpg"><br>
+        <img class="jotronlogo" src="Jotron_logo.jpg"><br>
         <div id=wrongpass></div>
         <div class="loginbox">
             Username<br />
