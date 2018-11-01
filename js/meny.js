42bace7688c61477b58d67f4abfe7421b3aa4015
shafique1 <43790388+shafique1@users.noreply.github.com>
2018-11-01 14:27:41 +0100

    meny
    meny

--- js/meny.js
+++ js/meny.js
@@ -1,11 +1,12 @@
 function showMeny() {
     document.getElementById('mainContent').innerHTML = `
+    <p class="m1">Menu</p>
 
-        <button class="login">Driver</button>
-        <button class="login">Passanger</button>
-        <button class="login">Profile</button>
-        <button class="login">History</button>
-        <button class="login">Log out</button>
+        <button class="m2">Driver</button>
+        <button class="m3">Passanger</button></br>
+        <button class="m4">Profile</button>
+        <button class="m5">History</button></br>
+        <button class="m6">Log out</button>
 
         `;
 }
