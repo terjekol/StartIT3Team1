3f4d9258097d0b8f21bf0e4e5c29f5fd3d970492
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-01 16:00:27 +0100

    Update meny.js

--- js/meny.js
+++ js/meny.js
@@ -3,7 +3,7 @@ function showMeny() {
     <div class="m1">Menu</div></br>
 
         <button class="m2">Driver</button>
-        <button class="m3">Passanger</button></br></br>
+        <button onclick="showPassengerAppointment()" class="m3">Passanger</button></br></br>
         <button class="m4">Profile</button>
         <button class="m5">History</button></br>
         <button class="m6">Log out</button>
