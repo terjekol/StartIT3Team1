04f4056051e2c2c42d4cee2111992a2c891ec9dc
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-01 15:25:48 +0100

    Create passApp.js

---
+++ passApp.js
@@ -0,0 +1,28 @@
+function showPassengerAppointment() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+    <div class="backtop"></div>
+    <a href="index.html"><div class="backbuttonsymbol">&lt;&lt</div></a>
+    <a href="index.html"><div class="backbutton"></div></a>
+
+    <div class="backheaddriverapp">Passenger</div>
+
+    <div class="appointmentsh1">Your active appointments</div>
+
+    <div class="appointmentbox1">Monday</div>
+    <div class="appointmentbox2">Tuesday</div>
+    <div class="appointmentbox3">Wednesday</div>
+    <div class="appointmentbox4">Thursday</div>
+    <div class="appointmentbox5">Friday</div>
+
+    <div class="weekback">&lt&lt</div>
+    <div class="weekback2"></div>
+    <div class="weeknext">&gt&gt</div>
+    <div class="weeknext2"></div>
+
+    <div class="colorexp"><b class="bcolor">Green</b> = Active appointment</div>
+    
+
+    <button class="finddriver" onclick="showDriverpage()">Find driver</button>
+ `;
+}
