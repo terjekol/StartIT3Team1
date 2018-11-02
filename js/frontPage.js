51ed5bc3059adfdfbe9842434e1820ea64dbbfed
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-02 11:19:00 +0100

    Update frontPage.js

--- js/frontPage.js
+++ js/frontPage.js
@@ -26,20 +26,3 @@ function showfrontPage() {
 
     `;
 }
-function dologInCheck() {
-    var username = document.getElementById('usernameTxt').value;
-    var password = document.getElementById('passwordTxt').value;
-    if (username == 'admin' && password == '12345') {
-        showMeny();
-    }
-    if (username == '' && password == '') {
-    }
-    else {
-        document.getElementById('wrongpass').innerHTML =
-            'Wrong Username or password!';
-        document.getElementById('usernameTxt').style.boxShadow = 
-            '0 0 20px red';
-        document.getElementById('passwordTxt').style.boxShadow = 
-            '0 0 20px red';
-    }
-}
