ba7fd9935d5effdd49d6536bfc67a55fad44426b
rawad87 <43792617+rawad87@users.noreply.github.com>
2018-11-02 10:28:41 +0100

    transport-request

---
+++ transport-request.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+function showTransportReguest() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+<div class="request">
+        Do you wish to send<br/>
+        Name <br/>
+        Transport Request<br/>
+
+
+        <button id="yesButton">Yes</button>
+        <button id="NoButton">No</button>
+
+    </div>`;
+}
