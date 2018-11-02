023499bc8fda5742b2596a4c071a224812cc4cf3
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-02 11:17:27 +0100

    Update cancelYesNo.js

--- js/cancelYesNo.js
+++ js/cancelYesNo.js
@@ -1,6 +1,7 @@
 function cancelYesNO() {
     var mainContentDiv = document.getElementById('mainContent');
     mainContentDiv.innerHTML = `
+
 <link rel="stylesheet" type="text/css" href="css.css">
     <div class="backtop"></div>
     <a href="index.html"><div class="backbuttonsymbol">&lt;&lt</div></a>
@@ -11,40 +12,5 @@ function cancelYesNO() {
         <button id="knappA">Yes</button>
         <button id="knappB">No</button>
     </div>
-    
-    
-    </div>
-    <style>
-        div.sikker {
-            color: white;
-        font-size: 34px;
-        margin-top: 20vh;
-        margin-bottom: 10vh;
-   
-    }
-        #knappA {
-            background - color: green;
-        border: groove black 2px;
-        border-radius: 10px;
-        box-shadow: 0 0 5px black;
-        color: black;
-        font-size: 32px;
-        text-align: center;
-        margin-top: 20px;
-        min-width: 170px;
-        max-width: 10vw;
-    }
-        #knappB {
-            background - color: red;
-        border: groove black 2px;
-        border-radius: 10px;
-        box-shadow: 0 0 5px black;
-        color: black;
-        font-size: 32px;
-        text-align: center;
-        margin-top: 20px;
-        min-width: 170px;
-        max-width: 10vw;
-        }
 `;
 }
