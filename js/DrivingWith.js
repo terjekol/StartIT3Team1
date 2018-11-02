b74da7f10f8236ca9ec379076b6552173825008c
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-02 11:15:07 +0100

    Update DrivingWith.js

--- js/DrivingWith.js
+++ js/DrivingWith.js
@@ -9,48 +9,5 @@ var mainContentDiv = document.getElementById('mainContent');
     <div class="knapp">
         <button id="knapp" onclick="sendsomewhere()">OK!</button>
     </div>
-    <style>
-        div.drive {
-            color: white;
-        font-size: 34px;
-        margin-top: 30vh;
-        margin-bottom:-35vh;
-    }
-
-        body {
-        background - color: #002147;
-        text-align: center;
-        font-family: 'Franklin Gothic Medium';
-        user-select: none;
-    }
-
-        #OK! {
-            background - color: white;
-        border: groove black 2px;
-        border-radius: 10px;
-        box-shadow: 0 0 5px black;
-        color: black;
-        font-size: 32px;
-        text-align: center;
-        margin-top: 10px;
-        min-width: 170px;
-        max-width: 10vw;
-        margin-bottom: 30vh;
-    }
-
-        #knapp {
-            background - color: white;
-        border: groove black 2px;
-        border-radius: 10px;
-        box-shadow: 0 0 5px black;
-        color: black;
-        font-size: 32px;
-        text-align: center;
-        margin-top: 40vh;
-        margin-bottom: 30vh;
-        min-width: 300px;
-        max-width: 10vw;
-    }
-    </style>
 `;
 }
