eaf5b79db0154334f2c917017691f78953e39a96
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-02 10:34:23 +0100

    Create transportRequest.js

---
+++ js/transportRequest.js
@@ -0,0 +1,11 @@
+function showTransportReguest() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+<div class="request">
+        Do you wish to send<br/>
+        Name <br/>
+        Transport Request<br/>
+        <button id="yesButton">Yes</button>
+        <button id="NoButton">No</button>
+    </div>`;
+}
