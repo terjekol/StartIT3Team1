1e8bce57e8158818b5d4bb179060e840ce6353a2
Zerones <43774584+Zerones@users.noreply.github.com>
2018-11-05 13:50:03 +0100

    Profilen er gjort om til CSS og JS, updaterte Index's med profile.js

---
+++ js/profile.js
@@ -0,0 +1,21 @@
+function profile()
+{
+    document.getElementById('mainContent').innerHTML = `    
+    <div class="backtop"></div>        
+    <div class="backheadpassreq">   Profile    </div>
+    <a href="index.html"><div class="backbuttonsymbol">&lt;&lt</div></a>
+    <a href="index.html"><div class="backbutton"></div></a>
+    <div class="lele2"><h2></h2></div>    
+    <div class="backgroundkaos"></div>      
+    <div class="profileBilde"><img src=img/bildetest.png style="max-width: 230px; max-height: 250px;"></div>
+    <div class="ProfileBoks">       
+            <div id="Navn" class="profileNavn">Houng Dajin</div>
+            <div class="profile1">Phone Number<br><div id="nummer" class="profileInfo">47 33 24 99</div></div>
+            <div class="profile2">Email <br><div id="mail" class="profileInfo">Tester@GetAcademy.no</div></div>
+            <div class="profile3">Pick up Point <br><div id="point" class="profileInfo">Torget 10 3269 LARVIK</div></div>
+            <div class="profile4">Driver Status<br><div id="status" class="profileInfo">Available</div></div>
+            <div id="lar"></div>
+            <div id="lar2"></div>
+            <div class="profile5">Edit Profile</div>
+    </div>`;
+}
