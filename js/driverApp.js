c460e87043e659d31d818f2404c8beae1bbf3649
Zerones <43774584+Zerones@users.noreply.github.com>
2018-11-05 10:45:03 +0100

    fiksa opp noe merge problemer og oppdaterte JS driver  pga. bildene flyttet til /img/. Profile er midletidig

--- js/driverApp.js
+++ js/driverApp.js
@@ -10,21 +10,21 @@ function driverApp() {
     <div class="driverboks">       
 
             <div class="lele">Date / Time</div>
-            <div class="driver3"><img src=bildetest.png class="bildete"></div>
+            <div class="driver3"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver4">Houng Dajin <br> Jonas Lies Vei 10C Larvik</div>
-            <div class="driver5"><img src=bildetest.png class="bildete"></div>
+            <div class="driver5"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver6">Mette-Marie Kristi <br> Nanset Gata 32 Larvik</div>
-            <div class="driver7"><img src=bildetest.png class="bildete"></div>
+            <div class="driver7"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver8">Elias Jacobsen <br> Hagalia 32 Larvik</div>
-            <div class="driver9"><img src=bildetest.png class="bildete"></div>
+            <div class="driver9"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver10">Hilde Mannegård <br> Torget 10 Larvik</div>
-            <div class="driver11"><img src=bildetest.png class="bildete"></div>
+            <div class="driver11">img src=img/bildetest.png class="bildete"></div>
             <div class="driver12">Tom .J. Bain <br> Bøkergata 21 Sandefjord</div>
-            <div class="driver13"><img src=bildetest.png class="bildete"></div>
+            <div class="driver13"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver14">Tommy Johnson <br> Øvre Ohlssons Gate 23 Tønsberg</div>
-            <div class="driver15"><img src=bildetest.png class="bildete"></div>
+            <div class="driver15"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver16">Thor Magne Lystfort <br> Gate Mc. Gataface Tønsberg</div>
-            <div class="driver17"><img src=bildetest.png class="bildete"></div>
+            <div class="driver17"><img src=img/bildetest.png class="bildete"></div>
             <div class="driver18">Ellen G. Hammsund <br> Tønsberg 10 Tønsberg</div>
     </div> 
     `;
