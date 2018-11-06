2f3e274dba944ef33765cd9c73b86ce5160af82f
Zebking <43807255+Zebking@users.noreply.github.com>
2018-11-06 14:58:29 +0100

    Update DrivingWith.js

--- js/DrivingWith.js
+++ js/DrivingWith.js
@@ -1,10 +1,10 @@
 function drivingWith () {
-var mainContentDiv = document.getElementById('mainContent');
     mainContentDiv.innerHTML = `
- <link rel="stylesheet" type="text/css" href="css.css">
+
     <div class="backtop"></div>
     <a href="index.html"><div class="backbuttonsymbol">&lt;&lt</div></a>
     <a href="index.html"><div class="backbutton"></div></a>
+
     <div class="drive">You are driving for: <br /> Navn Navnesen </div>
     <div class="knapp">
         <button id="knapp" onclick="sendsomewhere()">OK!</button>
