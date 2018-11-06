04ce1edad89300422b17ad375d4b8161b624eb23
shafique1 <43790388+shafique1@users.noreply.github.com>
2018-11-06 20:51:46 +0100

    ferdig med driver

---
+++ js/driver.js
@@ -0,0 +1,22 @@
+function showDriverAppointment() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+    <div class="backtop14"></div>
+    <div onclick="showMeny()" class="backbuttonsymbol16">&lt;&lt</div>
+    <div onclick="showMeny()" class="backbutton15"></div>
+    <div class="backheaddriverapp14">Driver</div>
+    <div class="d1">Your active appointments</div>
+    <div class="d2">Monday</div>
+    <div class="d3">Tuesday</div>
+    <div class="d4">Wednesday</div>
+    <div class="d5">Thursday</div>
+    <div class="d6">Friday</div>
+    <div class="weekback7">&lt&lt</div>
+    <div class="weeknext8">&gt&gt</div>
+    <div class="weekback9"></div>
+    <div class="weeknext10"></div>
+    <div class="color11"><b class="color12">Green</b> = Active appointment</div>
+    <button class="finddriver13" onclick="showDriverpage()">Find passenger</button>
+
+ `;
+}
--- js/meny.js
+++ js/meny.js
@@ -3,7 +3,7 @@ function showMeny() {
 
     <div class="m1">Menu</div></br>
 
-        <button class="m2">Driver</button>
+        <button onclick="showDriverAppointment()" class="m2">Driver</button>
         <button onclick="showPassengerAppointment()" class="m3">Passanger</button></br></br>
         <button class="m4">Profile</button>
         <button class="m5">History</button></br>
