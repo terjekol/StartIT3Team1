e44473ff6d98b5d0557466f26a7b104d74a19a2c
shafique1 <43790388+shafique1@users.noreply.github.com>
2018-11-07 08:48:43 +0100

    oppdatertdriver

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ function showMenu() {
     <div class="m1"></div>
     <div class="m11">Menu</div>
 
-    <button class="m2">Driver</button>
+    <button onclick="showDriver()" class="m2">Driver</button>
     <button onclick="showPassengerAppointment()" class="m3">Passanger</button><br>
     <button onclick="showProPage()" class="m4">Profile</button>
     <button onclick="showTheHistory()" class="m5">History</button><br>
