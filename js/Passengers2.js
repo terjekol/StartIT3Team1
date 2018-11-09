a8811834047d28c5d0f747939f379a51e9a7ad47
ChristianKjettorp <43775517+ChristianKjettorp@users.noreply.github.com>
2018-11-09 13:28:36 +0100

    Add files via upload

---
+++ js/Passengers2.js
@@ -0,0 +1,40 @@
+function passenger() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+
+
+<div class="backtop"></div>
+    <div class="backheadpassreq">   Passenger    </div>
+    <div onclick="showMenu()" class="backbuttonsymbol">&lt;&lt</div></a >
+        <div onclick="showMenu()" class="backbutton"></div></a >
+            <a href="index.html"><div class="backbuttonsymbol">&lt;&lt</div></a>
+            <a href="index.html"><div class="backbutton"></div></a>
+            <link rel="stylesheet" type="text/css" href="css.css">
+
+                <div class="m1"></div>
+                <div class="m11"></div>
+               
+            
+    <legend>Choose Date and Time</legend>
+                <div>
+                    <label for="drive-time">Date/time:</label>
+                    <input type="datetime-local" id="date-time"
+                        name="drive-time" value="2018-06-12T19:30"
+                        min="2018-06-07T00:00" max="2050-06-14T00:00" />
+                </div>
+
+                <div class="MyList">
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                    <div class="Navn"><img class="portrett1" src="https://dms-cf-03.dimu.org/image/032ynUtc35Jh?dimension=1200x1200" /><div>Navn Navnesen</div></div>
+                   </div>
+`;
+}
+
