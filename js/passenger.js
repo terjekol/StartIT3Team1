d471051f596cf5601eb629577e1a838d5f0d9fd3
rawad87 <43792617+rawad87@users.noreply.github.com>
2018-11-09 10:17:59 +0100

    diver history and passenger history are ready

--- js/passenger.js
+++ js/passenger.js
@@ -1,13 +1,11 @@
 function passengerHistory() {
     var mainContentDiv = document.getElementById('mainContent');
     mainContentDiv.innerHTML = `
-<div id="history1">History</div>
-    <div class="historyPassenger">
-        <svg id="icon2" onclick="homePage()" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M4 10v7h3v-7H4zm6 0v7h3v-7h-3zM2 22h19v-3H2v3zm14-12v7h3v-7h-3zm-4.5-9L2 6v2h19V6l-9.5-5z" fill="currentColor" />
-        </svg>
-    </div>
+<div class="backtop"></div>
+    <div onclick="showTheHistory()" class="backbuttonsymbol">&lt;&lt</div>
+    <div onclick="showTheHistory()" class="backbutton"></div>
+
+    <div class="backheaddriverapp">History</div>
     <div id="passenger">Passenger</div>
     <div id="firstpassenger">
         <div id="dates">22.11.2018</div>
