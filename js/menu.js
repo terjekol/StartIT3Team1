0756a7cf91ef487bde3e476c48c12a8ce345d4d3
Ravnster <magnus195@yahoo.no>
2018-11-11 03:14:04 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ function showMenu() {
     <div class="m11">Menu</div>
 
     <button onclick="showDriver()" class="m2">Driver</button>
-    <button onclick="showPassengerAppointment()" class="m3">Passanger</button><br>
+    <button onclick="showPassengerAppointment()" class="m3">Passenger</button><br>
     <button onclick="showProPage()" class="m4">Profile</button>
     <button onclick="showTheHistory()" class="m5">History</button><br>
 
